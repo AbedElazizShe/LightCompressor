e0e0be3bd33c1e3189d9ec9d4ee80ea970203856
dan3988 <40858622+dan3988@users.noreply.github.com>
2024-01-23 14:57:22 +0000

    Added constructor overloads for Configuration for backwards compatibility

--- lightcompressor/src/main/java/com/abedelazizshe/lightcompressorlibrary/config/Configuration.kt
+++ lightcompressor/src/main/java/com/abedelazizshe/lightcompressorlibrary/config/Configuration.kt
@@ -9,7 +9,36 @@ data class Configuration(
     var disableAudio: Boolean = false,
     val resizer: VideoResizer? = VideoResizer.auto,
     var videoNames: List<String>
-)
+) {
+    @Deprecated("Use VideoResizer to override the output video dimensions.", ReplaceWith("Configuration(quality, isMinBitrateCheckEnabled, videoBitrateInMbps, disableAudio, resizer = if (keepOriginalResolution) null else VideoResizer.auto, videoNames)"))
+    constructor(
+        quality: VideoQuality = VideoQuality.MEDIUM,
+        isMinBitrateCheckEnabled: Boolean = true,
+        videoBitrateInMbps: Int? = null,
+        disableAudio: Boolean = false,
+        keepOriginalResolution: Boolean,
+        videoNames: List<String>) : this(quality, isMinBitrateCheckEnabled, videoBitrateInMbps, disableAudio, getVideoResizer(keepOriginalResolution, null, null), videoNames)
+
+    @Deprecated("Use VideoResizer to override the output video dimensions.", ReplaceWith("Configuration(quality, isMinBitrateCheckEnabled, videoBitrateInMbps, disableAudio, resizer = VideoResizer.matchSize(videoWidth, videoHeight), videoNames)"))
+    constructor(
+        quality: VideoQuality = VideoQuality.MEDIUM,
+        isMinBitrateCheckEnabled: Boolean = true,
+        videoBitrateInMbps: Int? = null,
+        disableAudio: Boolean = false,
+        keepOriginalResolution: Boolean = false,
+        videoHeight: Double? = null,
+        videoWidth: Double? = null,
+        videoNames: List<String>) : this(quality, isMinBitrateCheckEnabled, videoBitrateInMbps, disableAudio, getVideoResizer(keepOriginalResolution, videoWidth, videoHeight), videoNames)
+}
+
+private fun getVideoResizer(keepOriginalResolution: Boolean, videoHeight: Double?, videoWidth: Double?): VideoResizer? =
+    if (keepOriginalResolution) {
+        null
+    } else if (videoWidth != null && videoHeight != null) {
+        VideoResizer.matchSize(videoWidth, videoHeight, true)
+    } else {
+        VideoResizer.auto
+    }
 
 data class AppSpecificStorageConfiguration(
     var subFolderName: String? = null,
